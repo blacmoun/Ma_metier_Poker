f341390af7d26f57c3142f2807731387de9b4fa6
Amin <Amin.deabreu@eduvaud.ch>
2026-01-19 09:31:12 +0100

    rename: view

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PlayerController;
 
 Route::get('/', function () {
-    return view('show');
+    return view('poker_table');
 });
 Route::resource('players', PlayerController::class)->only(['store']);
 
